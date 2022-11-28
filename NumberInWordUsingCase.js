6c7aea63fba479d2eef4a123bf20a252a4fd0839
usha-12 <usha12ahirwar@gmail.com>
2022-11-28 23:38:14 +0530

    read single digit number and write the number in word using case

---
+++ NumberInWordUsingCase.js
@@ -0,0 +1,37 @@
+var num = Math.floor(Math.random()*10);
+console.log(num);
+switch(num){
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    case 0:
+        console.log("Zero"); 
+        break;  
+    default:
+        console.log("Invalid");
+}
+/*Read a single digit number and write the number in word using Case*/
