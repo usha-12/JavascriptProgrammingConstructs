6a94a06c830e700567c37373a4b329311ebc2e5b
usha-12 <usha12ahirwar@gmail.com>
2022-11-28 23:23:38 +0530

    coin flip and print out heads or tails

---
+++ headtail.js
@@ -0,0 +1,7 @@
+const HEAD = 0;
+    let toss = Math.ceil(Math.random()*10)%2;
+    if(toss==HEAD)
+        console.log("Heads");
+    else 
+        console.log("Tails");
+/*Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly.*/
