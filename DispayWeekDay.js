1482197bdfa773513d3143663f68996797cc6a64
usha-12 <usha12ahirwar@gmail.com>
2022-11-28 23:32:46 +0530

    read a number and display the week day

---
+++ DispayWeekDay.js
@@ -0,0 +1,17 @@
+let day = Math.floor(Math.random()*10)%7+1;
+console.log(day)
+if(day==1)
+    console.log("Monday");
+else if(day==2)
+    console.log("Tuesday");
+else if(day==3)
+    console.log("Wednesday");
+else if(day==4)
+    console.log("Thursday");
+else if(day==5)
+    console.log("Friday");
+else if(day==6)
+    console.log("Saturday");
+else if(day==7)
+    console.log("Sunday");
+    /*Read a Number and Display the week day (Sunday, Monday,...)*/
