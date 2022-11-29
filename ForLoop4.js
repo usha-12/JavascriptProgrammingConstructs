9c1ee069246e30be5896fe2c8e39cc04c4c91bf9
usha-12 <usha12ahirwar@gmail.com>
2022-11-29 12:18:22 +0530

    factorial

---
+++ ForLoop4.js
@@ -0,0 +1,10 @@
+let num = Math.ceil(Math.random()*10); 
+console.log("Number = "+num);
+let factorial=1;
+for(let i=1;i<=num;i++){
+    factorial = factorial*i;
+}
+console.log("Factorial: "+factorial);
+/*Write a program that computes a factorial of a number taken as input.
+
+5 Factorial – 5! = 1 * 2 * 3 * 4 * 5*/
